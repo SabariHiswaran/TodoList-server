229c4bb8786ba9a41aae4df306155f5370edca69
Sabari <sabarihiswaran555@gmail.com>
2023-06-15 16:27:01 +0530

    updated port

---
+++ openaiapikey.js
@@ -0,0 +1,3 @@
+const api_Key = "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"
+
+module.exports = {api_Key}
--- server.js
+++ server.js
@@ -5,28 +5,40 @@ const cors = require('cors')
 
 const fetch = require('cross-fetch')
 
+const apiKey = require('./openaiapikey')
+
 const app = express()
 
-const port = process.env.PORT || 3000;
-
-// app.listen(port ,() => {
-//     console.log("server is listening in the port" , port)
-// })
+const port = process.env.PORT || 3005;
 
 app.use(cors())
 
 app.post('/', (req, res) => {
-    const { lat, lng } = req.query;
-    console.log(req.query);
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
+ 
+
+    console.log(req);
+
+    const url = "https://api.openai.com/v1/chat/completions";
   
     fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-      }
-    })
+        method: "POST",
+        headers: {
+         
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey.api_Key}`,
+          "Access-Control-Allow-Origin": "*",
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: `Write a usefull article regarding : ${req.todo}`,
+            },
+          ],
+        }),
+      })
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -41,3 +53,7 @@ app.post('/', (req, res) => {
         res.status(500).send('An error occurred');
       });
   });
+
+  app.listen(port ,() => {
+    console.log("server is listening in the port" , port)
+})
